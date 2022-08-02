5120c942fef4cd87a7e4183d861463c237b931f4
Karina Kieling <81115674+karinakieling@users.noreply.github.com>
2022-08-02 14:22:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Editor from "./components/editor/index";
+
+const App = () => {
+  return (
+    <div className="Container">
+      <h1>Editor de Texto</h1>
+      <Editor className="Editor" />
+    </div>
+  );
+};
+
+export default App;
